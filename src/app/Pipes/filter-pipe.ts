0c7450166ce68a6ec4d064362d502a0d1e56aae5
Stefan <sbrinton19@gmail.com>
2019-02-20 22:08:24 -0500

    Merging filters into table headers
    Also made fieldInfo Mandatory

--- src/app/Pipes/filter-pipe.ts
+++ src/app/Pipes/filter-pipe.ts
@@ -4,14 +4,13 @@ import { Recipe } from '../Classes/Recipe';
 import { FlatItem } from '../Classes/FlatItem';
 import { FlatSkill } from '../Classes/FlatSkill';
 import { FlatActivity } from '../Classes/FlatActivity';
-import { getAvailableTimes, getAvailableTimeName } from '../Enums/AvailableTime';
 @Pipe({
   name: 'filterStr'
 })
 
 
 export class FilterPipe implements PipeTransform {
-  transform(array: Array<any>, filter: string, fieldInfo?: string, tuple = false): Array<any> {
+  transform(array: Array<any>, filter: string, fieldInfo: string, tuple = false): Array<any> {
     if (array.length === 0) {
       return array;
     }
@@ -20,13 +19,13 @@ export class FilterPipe implements PipeTransform {
       array0 = array0[0];
     }
     if (array0 instanceof FlatPersona) {
-      return this.transformPersona(array, filter);
+      return this.transformPersona(array, filter, fieldInfo);
     } else if (array0 instanceof Recipe) {
       return this.transformRecipe(array, filter, fieldInfo);
     } else if (array0 instanceof FlatItem) {
-      return this.transformItem(array, filter);
+      return this.transformItem(array, filter, fieldInfo);
     } else if (array0 instanceof FlatSkill) {
-      return this.transformSkill(array, filter);
+      return this.transformSkill(array, filter, fieldInfo);
     } else if (array0 instanceof FlatActivity) {
       return this.transformActivity(array, filter, fieldInfo);
     } else {
@@ -35,12 +34,12 @@ export class FilterPipe implements PipeTransform {
     }
   }
 
-  private transformPersona(array: Array<FlatPersona>, filter: string): Array<FlatPersona> {
-    return array.filter(p => p.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+  private transformPersona(array: Array<FlatPersona>, filter: string, fieldInfo: string): Array<FlatPersona> {
+    return array.filter(p => p[fieldInfo].toLowerCase().indexOf(filter.toLowerCase()) !== -1);
   }
 
-  private transformItem(array: Array<FlatItem>, filter: string): Array<FlatItem> {
-    return array.filter(i => i.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+  private transformItem(array: Array<FlatItem>, filter: string, fieldInfo: string): Array<FlatItem> {
+    return array.filter(i => i[fieldInfo].toLowerCase().indexOf(filter.toLowerCase()) !== -1);
   }
 
   private transformRecipe(array: Array<Recipe>, filter: string, personaName: string): Array<Recipe> {
@@ -58,14 +57,14 @@ export class FilterPipe implements PipeTransform {
     });
   }
 
-  private transformSkill(array: Array<FlatSkill>, filter: string): Array<FlatSkill> {
-    return array.filter(s => s.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+  private transformSkill(array: Array<FlatSkill>, filter: string, fieldInfo: string): Array<FlatSkill> {
+    return array.filter(s => s[fieldInfo].toLowerCase().indexOf(filter.toLowerCase()) !== -1);
   }
 
   private transformActivity(array: Array<[FlatActivity, boolean]>, filter: string, fieldInfo: string): Array<[FlatActivity, boolean]> {
     array.forEach(tuple => {
       const activity = tuple[0];
-      tuple[1] = activity.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
+      tuple[1] = activity[fieldInfo].toLowerCase().indexOf(filter.toLowerCase()) !== -1;
     });
     return array;
   }
